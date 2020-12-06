c00724eded67ff1c71cffa0f28683ed3a4da4f89
Lucian Condrea <lucian@condrea.dev>
2020-12-06 11:34:48 -0500

    serve content through cdn, restrict bucket access

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,14 +1,18 @@
 import { Stack, StackProps, Construct, CfnOutput } from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as s3deploy from "@aws-cdk/aws-s3-deployment";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, "Bucket", {
-      websiteIndexDocument: "index.html",
-      publicReadAccess: true,
+    const bucket = new s3.Bucket(this, "Bucket");
+
+    // create CDN and point to s3 bucket
+    const cdn = new cloudfront.Distribution(this, "CloudFrontCDN", {
+      defaultBehavior: { origin: new origins.S3Origin(bucket) },
     });
 
     // upload html file to s3 bucket
@@ -17,6 +21,9 @@ export class CdkStack extends Stack {
       sources: [s3deploy.Source.asset("../web")],
     });
 
+    new CfnOutput(this, "CDNURL", {
+      value: cdn.distributionDomainName,
+    });
     new CfnOutput(this, "BucketURL", {
       value: bucket.bucketDomainName,
     });
