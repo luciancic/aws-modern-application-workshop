1e7b906d33ed8996bf6a8386fd436f042fd387d1
Lucian Condrea <lucian@condrea.dev>
2020-12-06 10:33:52 -0500

    add webpage and serve through s3

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,9 +1,24 @@
-import * as cdk from '@aws-cdk/core';
+import { Stack, StackProps, Construct, CfnOutput } from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "Bucket", {
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+    });
+
+    // upload html file to s3 bucket
+    new s3deploy.BucketDeployment(this, "Deployment", {
+      destinationBucket: bucket,
+      sources: [s3deploy.Source.asset("../web")],
+    });
+
+    new CfnOutput(this, "BucketURL", {
+      value: bucket.bucketDomainName,
+    });
   }
 }
