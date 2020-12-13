6e0570d9a7ef5f8650c4105cdd285c0a18de7108
Lucian Condrea <lucian@condrea.dev>
2020-12-12 19:00:45 -0500

    create a vpc

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,6 +2,8 @@
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { CdkStack } from "../lib/cdk-stack";
+import { NetworkStack } from "../lib/network-stack";
 
 const app = new cdk.App();
 new CdkStack(app, "misfits-bucket");
+const networkStack = new NetworkStack(app, "misfits-network");
