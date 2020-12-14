7dc483455110cd54a5b8e6d8395b98f1ee73e1f7
Lucian Condrea <lucian@condrea.dev>
2020-12-13 19:38:46 -0500

    push docker image to ecr through cdk

--- cdk/lib/ecr-stack.ts
+++ cdk/lib/ecr-stack.ts
@@ -1,12 +1,20 @@
-import { Stack, StackProps, Construct } from "@aws-cdk/core";
-import { Repository } from "@aws-cdk/aws-ecr";
+import { Stack, StackProps, Construct, CfnOutput } from "@aws-cdk/core";
+import { IRepository } from "@aws-cdk/aws-ecr";
+import { DockerImageAsset } from "@aws-cdk/aws-ecr-assets";
+import * as path from "path";
 
 export class EcrStack extends Stack {
-    public readonly ecrRepository: Repository;
+    public readonly ecrRepository: IRepository;
 
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        this.ecrRepository = new Repository(this, "misfits-repo");
+        const asset = new DockerImageAsset(this, "misfits-image", {
+            directory: path.join(__dirname, "../../app"),
+        });
+
+        this.ecrRepository = asset.repository;
+
+        new CfnOutput(this, "ecr-repo-name", { value: this.ecrRepository.repositoryName });
     }
 }
