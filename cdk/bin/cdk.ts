bf2b06a15def9dc583e1b4188d2868f084eb8404
Lucian Condrea <lucian@condrea.dev>
2020-12-21 09:13:42 -0500

    remove redundant stacks

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,15 +2,8 @@
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { CdkStack } from "../lib/cdk-stack";
-import { NetworkStack } from "../lib/network-stack";
-import { EcrStack } from "../lib/ecr-stack";
 import { EcsStack } from "../lib/ecs-stack";
 
 const app = new cdk.App();
 new CdkStack(app, "misfits-bucket");
-const networkStack = new NetworkStack(app, "misfits-network");
-const ecrStack = new EcrStack(app, "misfits-ecr");
-new EcsStack(app, "misfits-containers", {
-    vpc: networkStack.vpc,
-    ecrImage: ecrStack.ecrImage,
-});
+new EcsStack(app, "misfits-containers");
--- cdk/lib/ecs-stack.ts
+++ cdk/lib/ecs-stack.ts
@@ -1,87 +1,22 @@
 import { Construct, Stack, StackProps } from "@aws-cdk/core";
-import * as ec2 from "@aws-cdk/aws-ec2";
-import * as ecrAssets from "@aws-cdk/aws-ecr-assets";
 import * as ecs from "@aws-cdk/aws-ecs";
 import * as ecsPatterns from "@aws-cdk/aws-ecs-patterns";
-import * as iam from "@aws-cdk/aws-iam";
-
-interface EcsStackProps extends StackProps {
-    vpc: ec2.Vpc;
-    ecrImage: ecrAssets.DockerImageAsset;
-}
+import { DockerImageAsset } from "@aws-cdk/aws-ecr-assets";
+import * as path from "path";
 
 export class EcsStack extends Stack {
-    public readonly ecsCluster: ecs.Cluster;
-    public readonly ecsService: ecsPatterns.NetworkLoadBalancedFargateService;
-
-    constructor(scope: Construct, id: string, props: EcsStackProps) {
+    constructor(scope: Construct, id: string) {
         super(scope, id);
 
-        this.ecsCluster = new ecs.Cluster(this, "misfits-cluster", { vpc: props.vpc });
-        this.ecsCluster.connections.allowFromAnyIpv4(ec2.Port.tcp(8080));
+        const ecrImage = new DockerImageAsset(this, "misfits-image", {
+            directory: path.join(__dirname, "../../app"),
+        });
 
-        this.ecsService = new ecsPatterns.NetworkLoadBalancedFargateService(
-            this,
-            "misfits-service",
-            {
-                cluster: this.ecsCluster,
-                desiredCount: 1,
-                taskImageOptions: {
-                    image: ecs.ContainerImage.fromDockerImageAsset(props.ecrImage),
-                    containerPort: 8080,
-                },
-            }
-        );
-        this.ecsService.service.connections.allowFrom(
-            ec2.Peer.ipv4(props.vpc.vpcCidrBlock),
-            ec2.Port.tcp(8080)
-        );
-
-        const taskDefinitionPolicy = new iam.PolicyStatement();
-        taskDefinitionPolicy.addActions(
-            // Rules which allow ECS to attach network interfaces to instances
-            // on your behalf in order for awsvpc networking mode to work right
-            "ec2:AttachNetworkInterface",
-            "ec2:CreateNetworkInterface",
-            "ec2:CreateNetworkInterfacePermission",
-            "ec2:DeleteNetworkInterface",
-            "ec2:DeleteNetworkInterfacePermission",
-            "ec2:Describe*",
-            "ec2:DetachNetworkInterface",
-
-            // Rules which allow ECS to update load balancers on your behalf
-            //  with the information sabout how to send traffic to your containers
-            "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-            "elasticloadbalancing:DeregisterTargets",
-            "elasticloadbalancing:Describe*",
-            "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-            "elasticloadbalancing:RegisterTargets",
-
-            //  Rules which allow ECS to run tasks that have IAM roles assigned to them.
-            "iam:PassRole",
-
-            //  Rules that let ECS create and push logs to CloudWatch.
-            "logs:DescribeLogStreams",
-            "logs:CreateLogGroup"
-        );
-        taskDefinitionPolicy.addAllResources();
-
-        this.ecsService.service.taskDefinition.addToExecutionRolePolicy(taskDefinitionPolicy);
-
-        const taskRolePolicy = new iam.PolicyStatement();
-        taskRolePolicy.addActions(
-            // Allow the ECS Tasks to download images from ECR
-            "ecr:GetAuthorizationToken",
-            "ecr:BatchCheckLayerAvailability",
-            "ecr:GetDownloadUrlForLayer",
-            "ecr:BatchGetImage",
-            // Allow the ECS tasks to upload logs to CloudWatch
-            "logs:CreateLogStream",
-            "logs:CreateLogGroup",
-            "logs:PutLogEvents"
-        );
-        taskRolePolicy.addAllResources();
-
-        this.ecsService.service.taskDefinition.addToTaskRolePolicy(taskRolePolicy);
+        new ecsPatterns.ApplicationLoadBalancedFargateService(this, "misfits-cluster", {
+            taskImageOptions: {
+                image: ecs.ContainerImage.fromDockerImageAsset(ecrImage),
+                containerPort: 8080,
+            },
+        });
     }
 }
